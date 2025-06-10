8760ef4c0364741a8c80285a7d130d981cdf827c
davidrodriguez88 <davidxrodriguez@me.com>
2025-06-10 10:20:04 -0400

    Created Student class and properties

---
+++ GradeGuardian/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeGuardian;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ GradeGuardian/Student.cs
@@ -0,0 +1,16 @@
+﻿var Grades = new List<string>();
+
+namespace GradeGuardian
+{
+    public class Student
+    {
+        public Student()
+        {
+        }
+    
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
