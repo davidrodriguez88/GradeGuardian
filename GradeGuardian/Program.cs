d5b0ad36cef81f81406324055b7b9f1eb608a343
davidrodriguez88 <davidxrodriguez@me.com>
2025-06-11 01:33:23 -0400

    Created and populated list

--- GradeGuardian/Program.cs
+++ GradeGuardian/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       List<Student> students = new List<Student>(); 
+       
+       var student1 = new Student {Name = "Cartman", Id = 1};
+       var student2 = new Student { Name = "Stan", Id = 2 };
+       var student3 = new Student {Name = "Kyle", Id = 3};
+       var student4 = new Student {Name = "Kenny", Id = 4};
+       
+       students.Add(student1);
+       students.Add(student2);
+       students.Add(student3);
+       students.Add(student4);
+       
+       student1.AddGrade(65);
+       student1.AddGrade(44, 32, 40);
+
+       student2.AddGrade(75);
+       student2.AddGrade(88, 69, 70);
+       
+       student3.AddGrade(90);
+       student3.AddGrade(89.9, 94, 97);
+       
+       student4.AddGrade(64);
+       student4.AddGrade(72.6, 55, 68.3);
     }
+    
 }
